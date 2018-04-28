0d5d41537cd5385e2af64f7e50ab7c98226a5ebd
Arthur Christoph <arthur.christoph@gmail.com>
2018-04-28 00:50:17 -0500

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,30 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let dom: any;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      declarations: [AppComponent]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    dom = fixture.nativeElement;
+    component = fixture.componentInstance;
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  test('should exist', () => {
+    expect(component).toBeDefined();
+  });
+
+  test('should have a default title', () => {
+    expect(component.title).toBe('app');
+  });
+
+  test('should output Welcome to {{ title }}!', () => {
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    expect(dom.innerHTML).toContain('Welcome to app!');
+  });
 });
